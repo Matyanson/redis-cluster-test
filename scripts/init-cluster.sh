0ed612fa6e252d3eea3e2e1f1f96f90882fd986e
Matyanson <matyas.perem@seznam.cz>
2025-05-04 16:52:04 +0200

    feat: configure default user with ACL

--- scripts/init-cluster.sh
+++ scripts/init-cluster.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
+# exit on error
+set -e
+
+# admin credentials
+USER=default
+PASS=defaultpassword
+
 echo "Creating Redis Cluster..."
 
 # 3 masters (2 replicas for each)
 # 9 total / (2 replicas + 1 master) = 3 masters
-yes yes | redis-cli --cluster create \
+yes yes | redis-cli \
+  --user "$USER" \
+  -a "$PASS" \
+  --cluster create \
   172.28.0.2:6379 172.28.0.3:6379 172.28.0.4:6379 \
   172.28.0.5:6379 172.28.0.6:6379 172.28.0.7:6379 \
   172.28.0.8:6379 172.28.0.9:6379 172.28.0.10:6379 \
